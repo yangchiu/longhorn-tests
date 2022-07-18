5010fa5397580dc7c2339a943c88489806a4e148
Yang Chiu <yang.chiu@suse.com>
2022-07-18 11:44:23 +0800

    Add Rancher air gap installation code flow
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

---
+++ test_framework/scripts/rancher/terraform/variable.tf
@@ -0,0 +1,35 @@
+variable "api_url" {
+  type = string
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "longhorn_git_repo" {
+  type = string
+}
+
+variable "longhorn_git_branch" {
+  type = string
+}
+
+variable "registry_url" {
+  type = string
+}
+
+variable "registry_user" {
+  type = string
+}
+
+variable "registry_passwd" {
+  type = string
+}
+
+variable "registry_secret" {
+  type = string
+}

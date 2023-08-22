1abe1f175a674803647415a731a8c9583b70f9e4
Yang Chiu <yang.chiu@suse.com>
2023-08-22 15:14:14 +0800

    debug support bundle
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

--- test_framework/scripts/download-support-bundle.sh
+++ test_framework/scripts/download-support-bundle.sh
@@ -36,20 +36,25 @@ set_kubeconfig_envvar(){
 set_kubeconfig_envvar ${TF_VAR_arch} ${TF_VAR_tf_workspace}
 
 LH_FRONTEND_ADDR=`kubectl get svc -n longhorn-system longhorn-frontend -o json | jq -r '.spec.clusterIP + ":" + (.spec.ports[0].port|tostring)'`
+echo ${LH_FRONTEND_ADDR}
 
 JSON_PAYLOAD="{\"issueURL\": \"${SUPPORT_BUNDLE_ISSUE_DESC}\", \"description\": \"${SUPPORT_BUNDLE_ISSUE_DESC}\"}"
 
-CURL_CMD="curl -XPOST http://${LH_FRONTEND_ADDR}/v1/supportbundles -H 'Accept: application/json' -H 'Accept-Encoding: gzip, deflate' -d '"${JSON_PAYLOAD}"'"
+CURL_CMD="curl -s -XPOST http://${LH_FRONTEND_ADDR}/v1/supportbundles -H 'Accept: application/json' -H 'Accept-Encoding: gzip, deflate' -d '"${JSON_PAYLOAD}"'"
+echo ${CURL_CMD}
 
-SUPPORT_BUNDLE_URL=`kubectl exec -n longhorn-system svc/longhorn-frontend -- bash -c "${CURL_CMD}"  | jq -r '.links.self + "/" + .name'`
+SUPPORT_BUNDLE_RESP=`kubectl exec -n longhorn-system svc/longhorn-frontend -- bash -c "${CURL_CMD}"`
+echo ${SUPPORT_BUNDLE_RESP}
+SUPPORT_BUNDLE_URL=`echo ${SUPPORT_BUNDLE_RESP} | jq -r '.links.self + "/" + .name'`
+echo ${SUPPORT_BUNDLE_URL}
 
 SUPPORT_BUNDLE_READY=false
 while [[ ${SUPPORT_BUNDLE_READY} == false ]]; do
-    CMD_RESULT=`kubectl exec -n longhorn-system svc/longhorn-frontend -- curl -H 'Accept: application/json' ${SUPPORT_BUNDLE_URL}`
+    CMD_RESULT=`kubectl exec -n longhorn-system svc/longhorn-frontend -- curl -s -H 'Accept: application/json' ${SUPPORT_BUNDLE_URL}`
     PERCENT=`echo "${CMD_RESULT}" | jq -r '.progressPercentage' || true`
     echo ${PERCENT}
     
     if [[ ${PERCENT} == 100 ]]; then SUPPORT_BUNDLE_READY=true; fi
 done
 
-kubectl exec -n longhorn-system svc/longhorn-frontend -- curl -H 'Accept-Encoding: gzip, deflate' ${SUPPORT_BUNDLE_URL}/download > ${SUPPORT_BUNDLE_FILE_NAME}
+kubectl exec -n longhorn-system svc/longhorn-frontend -- curl -s -H 'Accept-Encoding: gzip, deflate' ${SUPPORT_BUNDLE_URL}/download > ${SUPPORT_BUNDLE_FILE_NAME}

446a7ab906edc489ac627088eb18fdb6e28c530a
Yang Chiu <yang.chiu@suse.com>
2024-03-05 14:25:09 +0800

    ci: always using stable version of kubectl when building test env docker image
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

--- test_framework/scripts/build.sh
+++ test_framework/scripts/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker build --no-cache -f ./test_framework/Dockerfile.setup -t "${JOB_BASE_NAME}-${BUILD_NUMBER}" .
+set -x
+
+docker build --no-cache -f ./test_framework/Dockerfile.setup --build-arg KUBECTL_VERSION="$(curl -L -s https://dl.k8s.io/release/stable.txt)" -t "${JOB_BASE_NAME}-${BUILD_NUMBER}" .

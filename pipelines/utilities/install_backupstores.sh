9ee6ce8d6d9280d960a7624db142338ec4857264
Yang Chiu <yang.chiu@suse.com>
2025-05-06 16:56:16 +0800

    test(robot): add Sync Up With Backup Target During DR Volume Activation test case
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

--- pipelines/utilities/install_backupstores.sh
+++ pipelines/utilities/install_backupstores.sh
@@ -3,7 +3,7 @@
 set -x
 
 install_backupstores(){
-  MINIO_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn-tests/master/manager/integration/deploy/backupstores/minio-backupstore.yaml"
+  MINIO_BACKUPSTORE_URL="https://raw.githubusercontent.com/yangchiu/longhorn-tests/refs/heads/dr-activation/manager/integration/deploy/backupstores/minio-backupstore.yaml"
   NFS_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn-tests/master/manager/integration/deploy/backupstores/nfs-backupstore.yaml"
   CIFS_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/backupstores/cifs-backupstore.yaml"
   AZURITE_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/backupstores/azurite-backupstore.yaml"

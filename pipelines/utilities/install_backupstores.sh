b88c84a48f2e8e1adffc9cb15bf1eac7c0e19525
Yang Chiu <yang.chiu@suse.com>
2025-08-14 15:02:53 +0800

    test minio
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

--- pipelines/utilities/install_backupstores.sh
+++ pipelines/utilities/install_backupstores.sh
@@ -3,7 +3,7 @@
 set -x
 
 install_backupstores(){
-  MINIO_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn-tests/master/manager/integration/deploy/backupstores/minio-backupstore.yaml"
+  MINIO_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn/8654b224418cc555b676185ab64c40eb4ec10015/deploy/backupstores/minio-backupstore.yaml"
   NFS_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn-tests/master/manager/integration/deploy/backupstores/nfs-backupstore.yaml"
   CIFS_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/backupstores/cifs-backupstore.yaml"
   AZURITE_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/backupstores/azurite-backupstore.yaml"

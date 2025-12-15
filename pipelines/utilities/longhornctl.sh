2aa29b06aba1d22ebf33bb9f2e59d29c2099ed30
Yang Chiu <yang.chiu@suse.com>
2025-12-15 11:13:34 +0800

    test latest cli
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

--- pipelines/utilities/longhornctl.sh
+++ pipelines/utilities/longhornctl.sh
@@ -1,5 +1,5 @@
 longhornctl_check(){
-  curl -L https://github.com/longhorn/cli/releases/download/v1.9.1/longhornctl-linux-amd64 -o longhornctl
+  curl -L https://github.com/longhorn/cli/releases/download/untagged-146fd4ea8ae53e56d2bf/longhornctl-linux-amd64 -o longhornctl
   chmod +x longhornctl
   ./longhornctl install preflight --enable-spdk
   ./longhornctl check preflight --enable-spdk
